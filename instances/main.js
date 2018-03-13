671b01d999cb77f40e1cfa5139c1d6a6c29e2e0a
Noémie Courtois <courtoisno@gmail.com>
2018-03-13 15:49:22 +0000

    Add files via upload

---
+++ instances/main.js
@@ -0,0 +1,93 @@
+
+
+$(document).ready(function(){
+
+
+
+	$(window).load(function() {
+	    $(".loader--headline").fadeOut(100000);
+	});
+
+
+
+  	$(".project1").css("display","none");
+
+    $('#p1').on('click', function(){
+ 		$('#PRO1').toggle(0);
+ 		
+	});
+
+
+   	$(".project2").css("display","none");
+   	$('#p2').on('click', function(){
+ 	   	$('#PRO2').toggle(0);
+	})
+
+
+   	$(".project3").css("display","none");
+   	$('#p3').on('click', function(){
+ 	   	$('#PRO3').toggle(0);
+	})
+
+
+
+   	$(".project4").css("display","none");
+   	$('#p4').on('click', function(){
+ 	   	$('#PRO4').toggle(0);
+	})
+
+
+   	$(".project5").css("display","none");
+   	$('#p5').on('click', function(){
+ 	   	$('#PRO5').toggle(0);
+	})
+
+
+
+   	$(".project6").css("display","none");
+   	$('#p6').on('click', function(){
+ 	   	$('#PRO6').toggle(0);
+	})
+
+
+   	$(".project7").css("display","none");
+   	$('#p7').on('click', function(){
+ 	   	$('#PRO7').toggle(0);
+	})
+
+
+   	$(".project8").css("display","none");
+   	$('#p8').on('click', function(){
+ 	   	$('#PRO8').toggle(0);
+	})
+
+
+   	$(".project9").css("display","none");
+   	$('#p9').on('click', function(){
+ 	   	$('#PRO9').toggle(0);
+	})
+
+	  	$(".project10").css("display","none");
+   	$('#p10').on('click', function(){
+ 	   	$('#PRO10').toggle(0);
+	})
+
+
+   	$(".project11").css("display","none");
+   	$('#p11').on('click', function(){
+ 	   	$('#PRO11').toggle(0);
+	})
+
+
+   	$(".project12").css("display","none");
+   	$('#p12').on('click', function(){
+ 	   	$('#PRO12').toggle(0);
+	})
+
+
+
+
+
+ });
+
+
