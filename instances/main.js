97ded089a56686c401d989254bd8b1338258d053
Noémie Courtois <courtoisno@gmail.com>
2018-03-14 16:06:15 +0000

    Add files via upload

--- instances/main.js
+++ instances/main.js
@@ -11,17 +11,28 @@ $(document).ready(function(){
 
 
   	$(".project1").css("display","none");
-
-    $('#p1').on('click', function(){
+   $('#p1').on('click', function(){
  		$('#PRO1').toggle(0);
  		
 	});
+
+      $(".project1").css("display","none");
+   $('.close1').on('click', function(){
+      $('#PRO1').toggle(0);
+      
+   });
 
 
    	$(".project2").css("display","none");
    	$('#p2').on('click', function(){
  	   	$('#PRO2').toggle(0);
 	})
+
+
+      $(".project2").css("display","none");
+      $('.close2').on('click', function(){
+         $('#PRO2').toggle(0);
+   })
 
 
    	$(".project3").css("display","none");
@@ -86,8 +97,73 @@ $(document).ready(function(){
 
 
 
+///HOVER////
 
 
- });
+
+   $('.hid1').css("display","none");
+   $('#p1').hover(function(){
+         $('.hid1').css("display","block");}, function(){
+            $('.hid1').css("display","none");
+         });
+
+
+   $('.hid2').css("display","none");
+   $('#p2').hover(function(){
+         $('.hid2').css("display","block");}, function(){
+            $('.hid2').css("display","none");
+         });
+
+
+   $('.hid3').css("display","none");
+   $('#p3').hover(function(){
+         $('.hid3').css("display","block");}, function(){
+            $('.hid3').css("display","none");
+         });
+
+
+   $('.hid4').css("display","none");
+   $('#p4').hover(function(){
+         $('.hid4').css("display","block");}, function(){
+            $('.hid4').css("display","none");
+         });
+  
+   $('.hid5').css("display","none");
+   $('#p5').hover(function(){
+         $('.hid5').css("display","block");}, function(){
+            $('.hid5').css("display","none");
+         });
+
+
+   $('.hid6').css("display","none");
+   $('#p6').hover(function(){
+         $('.hid6').css("display","block");}, function(){
+            $('.hid6').css("display","none");
+         });
+
+
+   $('.hid7').css("display","none");
+   $('#p7').hover(function(){
+         $('.hid7').css("display","block");}, function(){
+            $('.hid7').css("display","none");
+         });
+
+
+   $('.hid8').css("display","none");
+   $('#p8').hover(function(){
+         $('.hid8').css("display","block");}, function(){
+            $('.hid8').css("display","none");
+         });
+   });
+
+
+
+
+   $('.hid9').css("display","none");
+   $('#p9').hover(function(){
+         $('.hid9').css("display","block");}, function(){
+            $('.hid9').css("display","none");
+         });
+   
 
 
